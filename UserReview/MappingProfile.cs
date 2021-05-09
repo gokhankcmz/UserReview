0f3d6adc6d9632429719083481428c701b24b4c3
Gökhan Kocamaz <gk-89-@hotmail.com>
2021-05-09 20:05:12 +0300

    yok

---
+++ UserReview/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities;
+using Entities.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserReview
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Review, PublicReview>();
+        }
+    }
+}
