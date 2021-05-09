3b2f1dd12160212ee4ce29497d860f7953ecf1f0
Gökhan Kocamaz <gk-89-@hotmail.com>
2021-05-09 19:25:04 +0300

    yok

--- Entities/Configuration/ReviewConfiguration.cs
+++ Entities/Configuration/ReviewConfiguration.cs
@@ -19,8 +19,8 @@ namespace Entities
             {
                 Id = new Guid("41332237-ffc1-4c63-8edf-1fd1ec602473"),
                 CreatedAt = DateTime.Now,
-                content="Çamaşır makinesi aldım ama içinden çamaşır çıkmadı.",
-                title = "Aldatıcı satıcı.", 
+                content="A content",
+                title = "A title", 
                 star=1,
                 status = ReviewStatus.Approved,
                 userId= new Guid("f27f1c31-9b90-4406-8e7a-6dd1e6a7535c")
@@ -30,9 +30,8 @@ namespace Entities
             {
                 Id = new Guid("35d174b7-57b9-44bd-842a-4ee070b786bb"),
                 CreatedAt = DateTime.Now,
-                content = "Yemekler çok kötüydü ama kuryeniz çok nazik bir beyefendiydi" +
-                "sırf onun için 5 yıldız veriyorum bence kendisi iyi bir zammı hak ediyor.",
-                title = "Kurye çok iyi.",
+                content = "Another content",
+                title = "Another Title",
                 star = 5,
                 status = ReviewStatus.Pending,
                 userId = new Guid("f27f1c31-9b90-4406-8e7a-6dd1e6a7535c")
---
+++ Entities/DataTransferObjects/ReviewDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class PublicReview
+    {
+        public string content { get; set; }
+
+        public string title { get; set; }
+        public int star { get; set; }
+    }
+}
